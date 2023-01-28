5f60f898a33c3b972c93bfc562c29828023306dd
Killjoy <aryaajitnair@gmail.com>
2023-01-28 22:37:29 +0530

    Change nav links

--- frontend/src/components/property/ListingItem.js
+++ frontend/src/components/property/ListingItem.js
@@ -23,7 +23,7 @@ const ListingItem = (props) => {
                 },
             }}
             onClick={() => {
-                navigate(`/property/${id}`);
+                navigate(`/buyer/browse/${id}`);
             }}
         >
             <Box
