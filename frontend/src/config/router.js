3086acec23f9fd15d217ba28ac4574f6403e1cfb
Killjoy <aryaajitnair@gmail.com>
2023-01-28 21:44:18 +0530

    feat:Change router Auth

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -1,7 +1,7 @@
 import { Box } from "@mui/system";
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import Home from "../screens/Home";
-import Login from '../screens/Login'
+import Login from "../screens/Login";
 import Properties from "../screens/buyer/Properties";
 import MyProperties from "../screens/buyer/MyProperties";
 import PropertyDetails from "../screens/buyer/PropertyDetails";
@@ -15,65 +15,83 @@ import SellerDashboard from "../components/navbars/SellerDashboard";
 import AuthChecker from "../components/auth/AuthChecker";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
+    {
+        path: "/",
 
-		element: <Home />,
-		exact: true,
-		errorElement: <div>Hello</div>,
-	},
-	{
-		path: "/buyer",
-		element: (
-			<Box sx={{ display: "flex" }}>
-				<DashboardNavbar />
-				<Box>
-					<Outlet />
-				</Box>
-			</Box>
-		),
-		children: [
-			{ path: "/buyer/browse", element: <Properties />, exact: true },
-			{
-				path: "/buyer/browse/:propertyID",
-				element: <PropertyDetails />,
-				exact: true,
-			},
+        element: <Home />,
+        exact: true,
+        errorElement: <div>Hello</div>,
+    },
+    {
+        path: "/buyer",
+        element: (
+            <Box sx={{ display: "flex" }}>
+                <DashboardNavbar />
+                <Box>
+                    <Outlet />
+                </Box>
+            </Box>
+        ),
+        children: [
+            { path: "/buyer/browse", element: <Properties />, exact: true },
+            {
+                path: "/buyer/browse/:propertyID",
+                element: <PropertyDetails />,
+                exact: true,
+            },
 
-			{
-				path: "/buyer/my",
-				element: <MyProperties />,
-				exact: true,
-			},
-		],
-	},
-	{
-		path: "/seller",
-		element: (
-			<AuthChecker>
-				<Box display="flex">
-					<SellerDashboard />
-					<Outlet/>
-				</Box>
-			</AuthChecker>
-		),
-		children: [
-			{ path: "/seller/create", exact: true, element: <AddProperty /> },
-			{ path: "/seller/my", exact: true, element: <SellerPropertyDetails /> },
-		],
-	},
-	{
-		path: "/chat",
-		element: <Chat />,
-	},
-	{
-		path: "/admin",
-		element: <Admin />,
-	},
-	{
-		path:"/login",
-		element:<Login/>
-	}
+            {
+                path: "/buyer/my",
+                element: (
+                    <AuthChecker>
+                        <MyProperties />
+                    </AuthChecker>
+                ),
+                exact: true,
+            },
+        ],
+    },
+    {
+        path: "/seller",
+        element: (
+            <AuthChecker>
+                <Box display="flex">
+                    <SellerDashboard />
+                    <Outlet />
+                </Box>
+            </AuthChecker>
+        ),
+        children: [
+            { path: "/seller/create", exact: true, element: <AddProperty /> },
+            {
+                path: "/seller/my",
+                exact: true,
+                element: <SellerPropertyDetails />,
+            },
+        ],
+    },
+    {
+        path: "/chat",
+        element: (
+            <AuthChecker>
+                <Chat />
+            </AuthChecker>
+        ),
+    },
+    {
+        path: "/admin",
+		exact:true,
+        element: (
+            <AuthChecker>
+                <Admin />
+            </AuthChecker>
+        ),
+    },
+    {
+        path: "/login",
+		exact:true,
+        element: <Login />,
+    },
 ]);
 
 export default router;
