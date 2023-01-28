751f5e0c88f8453f2664ad34df7a41cd516ea7e6
Vatsal <bevatsal1122@gmail.com>
2023-01-28 21:34:08 +0530

    feat: resolves merge conflict

--- frontend/src/screens/Chat.js
+++ frontend/src/screens/Chat.js
@@ -1,35 +1,49 @@
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
-import { db, auth } from '../../config/firebase';
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { db, auth } from '../config/firebase';
 import { doc, getDoc } from "firebase/firestore";
-import NavLink from "./NavLink";
+// import NavLink from "./NavLink";
 import { useEffect, useState } from "react";
 import ChatIcon from '@mui/icons-material/Chat';
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const AllChatsOptions = (props) => {
     const [allChatter, setAllChatter] = useState([]);
     const [renderList, setRenderList] = useState();
     const navigate = useNavigate();
     const currentRoute = useLocation().pathname;
-
     useEffect(() => {
-      let userUIDs = props.allChatter;
-      // console.log(props)
-      let arr = [];  
-      const getData = async () => {
-          for (let i=0; i<userUIDs.length; ++i) {
-              const docRef = doc(db, "UserAuth", userUIDs[i]);
-              const docSnap = await getDoc(docRef);
-              // console.log("Document data:", docSnap.data());
-              arr.push(docSnap.data().name);
-          }
-          let i=-1;
-          setAllChatter(arr);
-          // console.log(arr);
-      }
-      getData();
-  }, [props.allChatter])
+        let userUIDs = props.allChatter;
+        // console.log(props)
+        let arr = [];  
+        const getData = async () => {
+            for (let i=0; i<userUIDs.length; ++i) {
+                const docRef = doc(db, "UserAuth", userUIDs[i]);
+                const docSnap = await getDoc(docRef);
+                // console.log("Document data:", docSnap.data());
+                arr.push(docSnap.data().name);
+            }
+            let i=-1;
+            setAllChatter(arr);
+            // console.log(arr);
+            // setRenderList(arr.map(doc => 
+            //     <NavLink
+            //     text={doc}
+            //     icon={<ChatIcon />}
+            //     key={doc} 
+                
+            //     onClickNavigateTo={`/chats?chatter=${userUIDs[++i]}`}
+            //     />
+            //     ))
+        }
+        getData();
+    }, [props.allChatter])
+
+    // console.log(allChatter);
 
     return (
         <Drawer
@@ -40,7 +54,10 @@ const AllChatsOptions = (props) => {
                 "& .MuiDrawer-paper": {
                     marginLeft: "350px",
                     marginTop: "4%",
-                    width: 340
+                    width: 280,
+                    boxSizing: "border-box",
+                    background: "rgba(252, 254, 254, 0.43)",
+                    backdropFilter: "blur(25px)",
                 },
             }}
             variant="permanent"
@@ -53,7 +70,11 @@ const AllChatsOptions = (props) => {
                         style={{
                             paddingLeft: "18px",
                             marginTop: "5px",
-                            marginBottom: "28px"
+                            marginBottom: "28px",
+                            paddingTop: "20%",
+                            cursor: "pointer",
+                            display: "flex",
+                            alignItems: "center",
                         }}
                     >
                     </div>
