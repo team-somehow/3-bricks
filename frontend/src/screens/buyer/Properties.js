f584dcd61537a5324b5acea1e64e5959cb967074
Vinay <vinaykanse9451@gmail.com>
2023-01-28 21:26:27 +0530

    feat: add search option for my properties

--- frontend/src/screens/buyer/Properties.js
+++ frontend/src/screens/buyer/Properties.js
@@ -5,65 +5,76 @@ import ListingItem from "../../components/property/ListingItem";
 import { db } from "../../config/firebase.js";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import SearchInput from "../../components/property/SearchInput";
 
 const Properties = () => {
-	const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
+    const [tempData, setTempData] = useState([]);
 
-	useEffect(() => {
-		const getProperties = async () => {
-			const snapshot = await getDocs(collection(db, "ListedProperties"));
-			let tData = [];
-			snapshot.forEach((doc) => {
-				// console.log(doc.id, " => ", doc.data());
-				let temp = doc.data();
+    useEffect(() => {
+        let tData = [];
+        const getProperties = async () => {
+            const snapshot = await getDocs(collection(db, "ListedProperties"));
+            snapshot.forEach((doc) => {
+                // console.log(doc.id, " => ", doc.data());
+                let temp = doc.data();
+                if (
+                    temp.authorize !== false &&
+                    temp.authorizeToSell !== false &&
+                    temp.alreadySold !== true
+                ) {
+                    tData.push({ ...doc.data(), id: doc.id });
+                }
+            });
+            setData(tData);
+            setTempData(tData);
+        };
+        getProperties();
+    }, []);
+    const updateProperties = (e) => {
+        const searchQuery = e.target.value;
 
-				if (
-					temp.authorize !== false &&
-					temp.authorizeToSell !== false &&
-					temp.alreadySold !== true
-				) {
-					tData.push({ ...doc.data(), id: doc.id });
-				}
-			});
-			setData(tData);
-		};
-		getProperties();
-	}, []);
-
-	return (
-		<Box m={5}>
-			<Box
-				component={Paper}
-				sx={{
-					width: "80%",
-					textAlign: "center",
-					borderRadius: "8px",
-					paddingY: "2px",
-					backgroundColor: "white",
-				}}
-			>
-				<Typography variant="h2" sx={{ margin: "5px" }}>
-					Availabe Properties
-				</Typography>
-			</Box>
-			<Box
-				width={"76vw"}
-				sx={{
-					display: "grid",
-					marginTop: "30px",
-					gridColumn: "3",
-					// gridGap: "10px",
-					gridTemplateColumns: "1fr 1fr 1fr",
-					// paddingX:"3%",
-					marginLeft: "-25px",
-				}}
-			>
-				{data.map((item) => (
-					<ListingItem {...item} key={item.id} />
-				))}
-			</Box>
-		</Box>
-	);
+        if (searchQuery.trim().length == 0) setData(tempData);
+        else
+            setData(
+                tempData.filter((element) => element.name.includes(searchQuery))
+            );
+    };
+    return (
+        <Box m={5}>
+            <Box
+                component={Paper}
+                sx={{
+                    width: "80%",
+                    textAlign: "center",
+                    borderRadius: "8px",
+                    paddingY: "2px",
+                    backgroundColor: "white",
+                }}
+            >
+                <Typography variant="h2" sx={{ margin: "5px" }}>
+                    Availabe Properties
+                </Typography>
+            </Box>
+            <SearchInput updateProperties={updateProperties} />
+            <Box
+                width={"76vw"}
+                sx={{
+                    display: "grid",
+                    marginTop: "30px",
+                    gridColumn: "3",
+                    // gridGap: "10px",
+                    gridTemplateColumns: "1fr 1fr 1fr",
+                    // paddingX:"3%",
+                    marginLeft: "-25px",
+                }}
+            >
+                {data.map((item) => (
+                    <ListingItem {...item} key={item.id} />
+                ))}
+            </Box>
+        </Box>
+    );
 };
 
 export default Properties;
